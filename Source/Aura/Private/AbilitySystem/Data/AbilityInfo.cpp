786d824ed865eda554d24e9cee00c5c3c9957a23
Thomas Rittener <raitener666@gmail.com>
2023-07-26 09:46:50 +0200

    section 20 - 5 à 10 min

---
+++ Source/Aura/Private/AbilitySystem/Data/AbilityInfo.cpp
@@ -0,0 +1,22 @@
+// Copyright Tryktel.
+
+
+#include "AbilitySystem/Data/AbilityInfo.h"
+
+FAuraAbilityInfo UAbilityInfo::FindAbilityInfoForTag(const FGameplayTag& AbilityTag, bool bLogNotFound) const
+{
+	for (const FAuraAbilityInfo& Info : AbilityInformation)
+	{
+		if (Info.AbilityTag == AbilityTag)
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+
+	{
+		
+	}
+	return FAuraAbilityInfo();
+}
